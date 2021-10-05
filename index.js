324b78a092dda8add3de6f8c3018816f82369609
Ramirez Nicolás <nicolas.ramirez@tradehelm.net>
2021-10-05 10:35:09 -0300

    simplify

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const { getOptions } = require('loader-utils');
 const { generateFonts } = require('fantasticon');
 const NativeModule = require('module');
-const serveStatic = require('serve-static');
-const connect = require('connect');
-const open = require('open');
-const path = require('path');
-const fs = require('fs');
 
 function wpGetOptions(context) {
   if (typeof context.query === 'string') {
@@ -14,22 +9,11 @@ function wpGetOptions(context) {
   return context.query;
 }
 
-function server(dir, name) {
-  const url = `http://localhost:8080/`;
-  connect()
-    .use(serveStatic(dir))
-    .listen(8080, () => {
-      console.log(`Server running on ${url} ...`);
-      open(url + name);
-    });
-}
-
 module.exports = function (content) {
   this.cacheable();
   const callback = this.async();
   const wOptions = wpGetOptions(this) || {};
   let rawFontConfig;
-  let htmlPath = '';
   try {
     rawFontConfig = JSON.parse(content);
   } catch (ex) {
@@ -41,38 +25,14 @@ module.exports = function (content) {
   }
 
   const { preview = false, ...options } = wOptions;
-
-  var fontConfig = Object.assign({}, options, rawFontConfig);
-  if (fontConfig.pathOptions) {
-    Object.values(fontConfig.pathOptions).forEach((entry) => {
-      const dir = path.dirname(entry);
-      if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true });
-      }
-    });
-    if (fontConfig.pathOptions.html) {
-      htmlPath = path.resolve(this.context, fontConfig.pathOptions.html);
-    }
-  }
+  const { onComplete = null, ...rawOptions } = rawFontConfig;
+  var fontConfig = Object.assign({}, options, rawOptions);
 
   console.log('> Compiling Fonts!');
   generateFonts(fontConfig)
     .then(() => {
-      if (preview && htmlPath) {
-        const dir = path.dirname(htmlPath);
-        const relative = path.relative(dir, fontConfig.outputDir);
-        if (relative && relative.startsWith('..')) {
-          const regex = new RegExp(fontConfig.name + '\\..*');
-          fs.readdirSync(fontConfig.outputDir).forEach((file) => {
-            if (regex.test(file)) {
-              fs.copyFileSync(
-                `${fontConfig.outputDir}/${file}`,
-                `${dir}/${file}`
-              );
-            }
-          });
-        }
-        server(dir, path.basename(htmlPath));
+      if (preview && onComplete) {
+        onComplete();
       }
       console.log('> Fonts Compiled!');
       callback(null, '');
